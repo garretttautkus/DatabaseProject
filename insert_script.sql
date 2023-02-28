1ddba38f376877667cb80b07de3fd29502d7f8d5
Ray Zhang <89791970+Eexey@users.noreply.github.com>
2023-02-28 13:11:49 -0700

    :DD

--- insert_script.sql
+++ insert_script.sql
@@ -1,18 +1,18 @@
 --Organization 
-INSERT INTO ORGANIZATION VALUES ("Super Incorporated", "109 N 100 W", "Provo", "Utah", 84663, "super_incorporated@gmail.com", "8018889011");
-INSERT INTO ORGANIZATION VALUES ("Pigeons United", "222 N 222 W", "Rexburg", "Idaho", 83646, "we_are_pigeons66@gmail.com", "2082222222");
+INSERT INTO ORGANIZATION VALUES ('Super Incorporated', '109 N 100 W', 'Provo', 'Utah', 84663, 'super_incorporated@gmail.com', '8018889011');
+INSERT INTO ORGANIZATION VALUES ('Pigeons United', '222 N 222 W', 'Rexburg', 'Idaho', 83646, 'we_are_pigeons66@gmail.com', '2082222222');
 
 
 --Hotel 
-INSERT INTO HOTEL VALUES (100, 'Hilton Hotel Missoula', 'Montana', 59901, '100 Madison St, Missoula, MT 59802', 4067283100, 4067283198, 200, 'Missoula');
-INSERT INTO HOTEL VALUES ("10000", "Hotel California", "California", 90001, "3832 Warbler St, Los Angeles, CA 90001", 3105432189, 500, "Los Angeles");
+INSERT INTO HOTEL VALUES (100, 'Hilton Hotel Missoula', 'Montana', 59901, '100 Madison St, Missoula, MT 59802', '4067283100', '4067283198', 200, 'Missoula');
+INSERT INTO HOTEL VALUES (10000, 'Hotel California', 'California', 90001, '3832 Warbler St, Los Angeles, CA 90001', '4001002000', '3105432189', 500, 'Los Angeles');
 
 
 --Conference
-INSERT INTO CONFERENCE VALUES ("IPOAC Year in Review", '2023-02-28', '2023-02-29', "Missoula", 25, 95, 2000, "Hilton Hotel Missoula");
-INSERT INTO CONFERENCE VALUES ("Uprising of the Avians", '2023-02-28', '2023-02-29', "Los Angeles", 25, 95, 2000, "Hotel California");
+INSERT INTO CONFERENCE VALUES ('IPOAC Year in Review', '2023-02-28', '2023-02-29', 'Missoula', 25, 95, 2000, 'Hilton Hotel Missoula');
+INSERT INTO CONFERENCE VALUES ('Uprising of the Avians', '2023-02-28', '2023-02-29', 'Los Angeles', 25, 95, 2000, 'Hotel California');
 
 
 --Attendee
-INSERT INTO ATTENDEE VALUES ("Paul Bunyan", 649100200, 10, "paul_bunyan@yahoo.com", 5, "Hilton Hotel Missoula");
-INSERT INTO ATTENDEE VALUES ("Acanthis", 9991006542, 40, "godofpigeons@pigeon.com", 0, "Hotel California");
+INSERT INTO ATTENDEE VALUES ('Paul Bunyan', 649100200, 10, 'paul_bunyan@yahoo.com', 5, 'Hilton Hotel Missoula');
+INSERT INTO ATTENDEE VALUES ('Acanthis', 9991006542, 40, 'godofpigeons@pigeon.com', 0, 'Hotel California');
