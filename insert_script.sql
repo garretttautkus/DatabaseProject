522801b2b0858267cb5259552e9bfe4267e2f48d
Ray Zhang <89791970+Eexey@users.noreply.github.com>
2023-02-28 13:18:09 -0700

    :DDDD

--- insert_script.sql
+++ insert_script.sql
@@ -4,13 +4,13 @@ INSERT INTO ORGANIZATION (OName, OAddress, OCity, OState, OZip, OEmail, OPhone) 
 
 
 --Hotel 
-INSERT INTO HOTEL (HMaxMeetSize, HName, HState, HZip, Haddress, HContactPhone, HPhone, HNumRooms, HCity) VALUES (100, 'Hilton Hotel Missoula', 'Montana', 59901, '100 Madison St, Missoula, MT 59802', '4067283100', '4067283198', 200, 'Missoula');
-INSERT INTO HOTEL (HMaxMeetSize, HName, HState, HZip, Haddress, HContactPhone, HPhone, HNumRooms, HCity) VALUES (10000, 'Hotel California', 'California', 90001, '3832 Warbler St, Los Angeles, CA 90001', '4001002000', '3105432189', 500, 'Los Angeles');
+INSERT INTO HOTEL (HMaxMeetSize, HName, HState, HZip, HAddress, HContactPhone, HPhone, HNumRooms, HCity) VALUES (100, 'Hilton Hotel Missoula', 'Montana', 59901, '100 Madison St, Missoula, MT 59802', '4067283100', '4067283198', 200, 'Missoula');
+INSERT INTO HOTEL (HMaxMeetSize, HName, HState, HZip, HAddress, HContactPhone, HPhone, HNumRooms, HCity) VALUES (10000, 'Hotel California', 'California', 90001, '3832 Warbler St, Los Angeles, CA 90001', '4001002000', '3105432189', 500, 'Los Angeles');
 
 
 --Conference
-INSERT INTO CONFERENCE (CNAme, CStartDate, CEndDate, CCIty, CFee, CAttendance, CBudget, HName) VALUES ('IPOAC Year in Review', '2023-02-28', '2023-02-29', 'Missoula', 25, 95, 2000, 'Hilton Hotel Missoula');
-INSERT INTO CONFERENCE (CNAme, CStartDate, CEndDate, CCIty, CFee, CAttendance, CBudget, HName) VALUES ('Uprising of the Avians', '2023-02-28', '2023-02-29', 'Los Angeles', 25, 95, 2000, 'Hotel California');
+INSERT INTO CONFERENCE (CName, CStartDate, CEndDate, CCIty, CFee, CAttendance, CBudget, HName) VALUES ('IPOAC Year in Review', '2023-02-28', '2023-02-29', 'Missoula', 25, 95, 2000, 'Hilton Hotel Missoula');
+INSERT INTO CONFERENCE (CName, CStartDate, CEndDate, CCIty, CFee, CAttendance, CBudget, HName) VALUES ('Uprising of the Avians', '2023-02-28', '2023-02-29', 'Los Angeles', 25, 95, 2000, 'Hotel California');
 
 
 --Attendee
