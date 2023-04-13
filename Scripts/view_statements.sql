63deec7ec1681d96d72b811ea3c96dc095119f41
Ray Zhang <89791970+Eexey@users.noreply.github.com>
2023-04-13 08:46:14 -0600

    updated the view statement again

--- Scripts/view_statements.sql
+++ Scripts/view_statements.sql
@@ -3,7 +3,7 @@ SELECT OID, OName, OEmail, OCity, OPhone
 FROM Organization;
 
 CREATE VIEW meeting_information AS
-SELECT OID, CID, CName, CAttendance, CStartDate, CEndDate, conference.HName, HState, HCity, HZip, HAddress
+SELECT Organization.OID, CID, CName, CAttendance, CStartDate, CEndDate, conference.HName, HState, HCity, HZip, HAddress
 FROM Conference
 JOIN Hotel ON Conference.HName = Hotel.HName
 JOIN Organization ON Conference.OID = Organization.OID;
