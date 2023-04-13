047b88f4138cc167ceaf1b7241f6f4426010126f
garretttautkus <89819190+garretttautkus@users.noreply.github.com>
2023-04-12 18:17:35 -0600

    CountingStars_OneRepublic

--- Website/frontend/src/pages/updateForm.jsx
+++ Website/frontend/src/pages/updateForm.jsx
@@ -22,6 +22,72 @@ const Update = () => {
         console.error(err.message); //console log the error
     }
   };
+  const handleSubmit2 = async (e) => {
+    
+    try {
+      await axios.put(`http://localhost:8080/updateStart/${cid}`, newForm);
+        setNewForm({ 
+          cstartdate : ""
+         }); // reset the form
+    } catch(err) {
+        console.error(err.message); //console log the error
+    }
+  };
+  const handleSubmit3 = async (e) => {
+    
+    try {
+      await axios.put(`http://localhost:8080/updateEnd/${cid}`, newForm);
+        setNewForm({ 
+          cstartdate : ""
+         }); // reset the form
+    } catch(err) {
+        console.error(err.message); //console log the error
+    }
+  };
+  const handleSubmit4 = async (e) => {
+    
+    try {
+      await axios.put(`http://localhost:8080/updateCity/${cid}`, newForm);
+        setNewForm({ 
+          cstartdate : ""
+         }); // reset the form
+    } catch(err) {
+        console.error(err.message); //console log the error
+    }
+  };
+  const handleSubmit5 = async (e) => {
+    
+    try {
+      await axios.put(`http://localhost:8080/updateFee/${cid}`, newForm);
+        setNewForm({ 
+          cstartdate : ""
+         }); // reset the form
+    } catch(err) {
+        console.error(err.message); //console log the error
+    }
+  };
+  const handleSubmit6 = async (e) => {
+    
+    try {
+      await axios.put(`http://localhost:8080/updateAttendance/${cid}`, newForm);
+        setNewForm({ 
+          cstartdate : ""
+         }); // reset the form
+    } catch(err) {
+        console.error(err.message); //console log the error
+    }
+  };
+  const handleSubmit7 = async (e) => {
+    
+    try {
+      await axios.put(`http://localhost:8080/updateBudget/${cid}`, newForm);
+        setNewForm({ 
+          cstartdate : ""
+         }); // reset the form
+    } catch(err) {
+        console.error(err.message); //console log the error
+    }
+  };
   const handleChange = (e) => {
     if (e.target.name === 'budget' && e.target.value === '') {
       setNewForm(prev => ({...prev, [e.target.name]: 0}));
@@ -48,7 +114,7 @@ const Update = () => {
             <br />
             <br />
 
-            {/* <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit2}>
               <label htmlFor="cstart">Conference Start Date</label>
               <input type="date" id="cstartdate" name="cstartdate" placeholder="Start Date" dateFormat="yyyy-mm-dd" onChange={handleChange} />
               <input type="submit" value="Update Start Date" />
@@ -57,7 +123,7 @@ const Update = () => {
             <br />
             <br />
 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit3}>
               <label htmlFor="cend">Conference End Date</label>
               <input type="date" id="cenddate" name="cenddate" placeholder="End Date" dateFormat="yyyy-mm-dd" onChange={handleChange} />
               <input type="submit" value="Update End Date" />
@@ -66,7 +132,7 @@ const Update = () => {
             <br />
             <br />
 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit4}>
               <label htmlFor="ccity">Conference City</label>
               <input type="text" id="ccity" name="ccity" placeholder="Conference City" onChange={handleChange} />
               <input type="submit" value="Update City" />
@@ -75,7 +141,7 @@ const Update = () => {
             <br />
             <br />
 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit5}>
               <label htmlFor="cfee">Fee</label>
               <input type="text" id="cfee" name="cfee" placeholder="Conference Attendance Fee" onChange={handleChange} />
               <input type="submit" value="Update Fee" />
@@ -84,7 +150,7 @@ const Update = () => {
             <br />
             <br />
 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit6}>
               <label htmlFor="cattendance">Conference Attendance</label>
               <input type="text" id="cattendance" name="cattendance" placeholder="Estimated Conference Attendance" onChange={handleChange} />
               <input type="submit" value="Update Attendance" />
@@ -93,11 +159,11 @@ const Update = () => {
             <br />
             <br />
                 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit7}>
               <label htmlFor="budget">Proposed Budget</label>
               <input type="number" id="cbudget" name="cbudget" placeholder="Proposed Budget" step="0.01" onChange={handleChange} />
               <input type="submit" value="Update Budget" />
-            </form>   */}
+            </form>  
             
           </div>
         </div>
